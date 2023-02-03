7809e2e9186775070dec7300818bc6ca91547e19
MagdalenaBernat <magdalena.bernat@onet.pl>
2023-02-03 15:29:16 +0100

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -7,16 +7,21 @@ const API_VERSION = "v3.1";
 function fetchCountries(name) {
     return fetch(`${COUNTRIES_API_URL}/${API_VERSION}/name/${name}?fields=name,capital,population,flags,languages`)
         .then((response) => {
-            if (response.ok) {
-                return response.json();
-    } else if(response.status === 404) {
-       Notiflix.Notify.failure('Oops, there is no country with that name');
-    return Promise.reject('error 404')
-    } else {
-      return Promise.reject('some other error: ' + response.status)
-    }
+            if (response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    if (response.status === 404) {
+                        Notiflix.Notify.failure('Oops, there is no country with that name');
+                        return Promise.reject('error 404')
+                    } else {
+                        return Promise.reject('some other error: ' + response.status)
+                    }
+                }
+            }
         })
         .catch((error) => {
             console.log("Error: ", error);
         })
 };
+
--- src/index.js
+++ src/index.js
@@ -10,46 +10,61 @@ const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector("#search-box");
 const countryList = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info");
-
+let countriesListHTML = "";
 
 const countryListElement = country => {
-    countryInfo.innerHTML = "";
-    countryList.insertAdjacentHTML(
-      'beforeend', 
-    `
-    <li><img src="${country.flags.svg}"/><h4>${country.name.official}</h4></li>
-    `
-  );
+    li = document.createElement('li');
+    img = document.createElement('img');
+    heading = document.createElement('h4');
+    img.src = country.flags.svg;
+    heading.innerHTML = country.name.official;
+    li.appendChild(img);
+    li.appendChild(heading);
+    countryList.appendChild(li);
 };
 
 const countryInfoDetails = countries => {
-    countryList.innerHTML = "";
+    clearCode();
     countryInfo.innerHTML = `
         <img src="${countries[0].flags.svg}"/><h3>${countries[0].name.official}</h3>
         <p><span class="bold">Capital:</span> ${countries[0].capital}</p>
         <p><span class="bold">Population:</span> ${countries[0].population}</p>
         <p><span class="bold">Languages:</span> ${Object.values(countries[0].languages)}</p>                 
     `;
+
 };
 
+const clearCode = () => { countryList.innerHTML = "" };
+
 function printCountries(countries) {
-    let countriesListHTML = "";
-    if (countries.length === 1) {
-        countriesListHTML += countryInfoDetails(countries);
-    } else if (countries.length >= 10) {
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-    } else {
-        countries.forEach((country) => {
-            countriesListHTML += countryListElement(country);
-        }) 
-    } 
+    countriesListHTML = "";
+    if (countries) {
+        if (countries.length === 1) {
+            clearCode();
+            countryInfoDetails(countries);
+        } else if (countries.length >= 10) {
+            countryInfo.innerHTML = "";
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        } else {
+            clearCode();
+            countryInfo.innerHTML = "";
+            let li = "";
+            let img = "";
+            let heading = "";
+            countries.forEach((country) => {
+                countryListElement(country);
+            })
+        }
+    }
 };
 
 searchBox.addEventListener("input", _.debounce(() => {
     fetchCountries(searchBox.value.trim())
-        .then(countries => { printCountries(countries) });
-    countryList.insertAdjacentHTML('beforeend', countriesListHTML);
-    countryInfo.innerHTML = countriesListHTML;
+        .then(countries => {
+            if (countries) {
+            printCountries(countries)  
+            }
+        });
 }, DEBOUNCE_DELAY, {
     leading: false,
     trailing: true,
@@ -58,43 +73,6 @@ searchBox.addEventListener("input", _.debounce(() => {
 
 searchBox.addEventListener("keydown", (event) => {
     if (searchBox.textContent = "" || event.code === "Backspace" || event.code === "Delete") {
-        countriesListHTML = "";
+        clearCode();
     }
 });
-  
-
-// function printCountries(countries) {
-//     let countriesListHTML = "";
-//     if (countries.length === 1) {
-//         countriesListHTML += `
-//             <img src="${countries[0].flags.svg}"/><h3>${countries[0].name.official}</h3> 
-//             <p>Capital: ${countries[0].capital}</p>
-//             <p>Population: ${countries[0].population}</p>
-//             <p>Languages: ${Object.values(countries[0].languages)}</p>                   
-//     `
-//     } else if (countries.length >= 10) {
-//         Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-//     } else {
-//         countries.forEach((country) => {
-//             countriesListHTML += `
-//             <img src="${country.flags.svg}"/><h4>${country.name.official}</h4> 
-//         `
-//         })
-//     }
-//         countryList.innerHTML = countriesListHTML;
-// };
-
-// searchBox.addEventListener("input", _.debounce(() => {
-//     fetchCountries(searchBox.value.trim())
-//         .then(countries => { printCountries(countries) });
-// }, DEBOUNCE_DELAY, {
-//     leading: false,
-//     trailing: true,
-// }));
-
-// function clearOutput(){
-//     if (searchBox.textContent = "") {
-//         countriesListHTML = "";
-//     }
-// return
-// };
